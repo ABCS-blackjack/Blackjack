05496731a90064f118cf53190c579f0558622559
chrishome <chrisware707@gmail.com>
2020-04-01 20:41:53 -0700

    cleanup

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,48 +16,56 @@ import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-        Integer playerValue = 0;
+    Integer playerValue = 0;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final Deck singleDeck = new Deck(1);
-        Collections.shuffle (singleDeck.myDeck);
+        Collections.shuffle(singleDeck.myDeck);
 
         final ImageView playerHand = findViewById(R.id.playerCard);
+        final ImageView dealerHand = findViewById(R.id.dealerCard);
+
         final TextView playerCount = findViewById(R.id.playerCount);
-        Button test = findViewById(R.id.buttonTest);
 
-        test.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                        playerHand.setImageResource(singleDeck.myDeck.get(0).getDrawable());
-                        playerValue = playerValue + singleDeck.myDeck.get(0).getValue();
-                        playerCount.setText(String.valueOf(playerValue));
-                        singleDeck.myDeck.remove(0);
-
-                }
-        });
+        Button hitButton = findViewById(R.id.buttonHit);
+        Button standButton = findViewById(R.id.buttonStand);
+        Button DDButton = findViewById(R.id.buttonDD);
+        Button splitButton = findViewById(R.id.buttonSplit);
 
         ImageButton settingsActivity = findViewById(R.id.imageSettingButton);
-        settingsActivity.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                        Intent nextPage = new Intent(MainActivity.this, SettingsActivity.class);
-                        startActivity(nextPage);
-                }
+        ImageButton analyzeActivity = findViewById(R.id.imageAnalyzeButton);
+
+
+        hitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                playerHand.setImageResource(singleDeck.myDeck.get(0).getDrawable());
+                playerValue = playerValue + singleDeck.myDeck.get(0).getValue();
+                playerCount.setText(String.valueOf(playerValue));
+                singleDeck.myDeck.remove(0);
+
+            }
         });
 
-        ImageButton analyzeActivity = findViewById(R.id.imageAnalyzeButton);
-        analyzeActivity.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                        Intent nextPage = new Intent(MainActivity.this, AnalyzeActivity.class);
-                        startActivity(nextPage);
-                }
+        settingsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextPage = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(nextPage);
+            }
         });
-        }
+
+        analyzeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextPage = new Intent(MainActivity.this, AnalyzeActivity.class);
+                startActivity(nextPage);
+            }
+        });
+    }
 }
