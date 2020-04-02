d091a764dfea4f0118c9945d68eb2793a2d5039f
chrishome <chrisware707@gmail.com>
2020-04-01 19:54:14 -0700

    add text view that adds the value of the cards taken out

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,41 +1,43 @@
 package com.example.myapplication;
 
-import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
+        Integer playerValue = 0;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Deck singleDeck = new Deck();
+        final Deck singleDeck = new Deck(1);
         Collections.shuffle (singleDeck.myDeck);
-        System.out.println(singleDeck.myDeck.size());
 
-        final ImageView testImage = findViewById(R.id.playerCard);
+        final ImageView playerHand = findViewById(R.id.playerCard);
+        final TextView playerCount = findViewById(R.id.playerCount);
         Button test = findViewById(R.id.buttonTest);
 
         test.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                        //Drawable.createFromPath(singleDeck.getValue());
-                        testImage.setImageResource(singleDeck.myDeck.get(0));
+
+                        playerHand.setImageResource(singleDeck.myDeck.get(0).getDrawable());
+                        playerValue = playerValue + singleDeck.myDeck.get(0).getValue();
+                        playerCount.setText(String.valueOf(playerValue));
                         singleDeck.myDeck.remove(0);
+
                 }
         });
 
